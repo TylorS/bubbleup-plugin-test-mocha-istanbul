c7aa058d6ce4ad6f8c74760e6c8e22a610337de8
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-10 01:04:48 -0400

    fix(reporter): also output results of tests using spec reporter as well

--- bubbleup-plugin-test-mocha-istanbul/index.js
+++ bubbleup-plugin-test-mocha-istanbul/index.js
@@ -5,6 +5,7 @@ process.env.ISTANBUL_REPORT_DIR = 'coverage'
 module.exports = {
   exec: function (mocha, register) {
     mocha.reporter('mocha-istanbul')
+    mocha.reporter('spec')
   },
   base: false
 }
